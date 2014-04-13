8f0b1b2c63a7dba12e2a6af6221feed4cd5e1a58
themetalmuncher <themetalmuncher@gmail.com>
2014-04-13 21:08:28 +0100

    [removed] nonfunctional fuel economy code, todo

--- apps/SimpleEngineDebug/app.js
+++ apps/SimpleEngineDebug/app.js
@@ -4,14 +4,6 @@ SimpleEngineDebug.prototype.initialize = function(){
     this.text = $('<div style="background:RGBA(255,255,255,0.75);padding:5px;"></div>').appendTo(this.rootElement);
 };
 
-oldtime = 0;
-newtime = 0;
-timer = 1;
-
-previousfuel = 0;
-fuelconsumptionrate = 0;
-distance = 0;
-
 SimpleEngineDebug.prototype.update = function(streams){
     
     str =  "Engine RPM: " + streams["engineInfo"][4].toFixed();
@@ -30,24 +22,6 @@ SimpleEngineDebug.prototype.update = function(streams){
     str += "<br> Power: " + (streams["engineInfo"][4]*streams["engineInfo"][9]*Math.PI/30000).toFixed(2) + " kW" ;
     str += "<br> Airspeed: " + (streams["electrics"].airspeed*3.6).toFixed(2) + " km/h";
     str += "<br> Fuel: " + streams["engineInfo"][11].toFixed(2) + " L / " + streams["engineInfo"][12].toFixed(2) + " L, " + ((streams["engineInfo"][11]/streams["engineInfo"][12])*100.).toFixed(2) + "%"
-    
-    oldtime = newtime;
-    newtime = performance.now()/1000;
-    dt = newtime-oldtime;
-    
-    timer = timer - dt;
-    if (timer < 0) {
-        if (previousfuel > streams["engineInfo"][11]){
-            distance = (1-timer) * streams["electrics"].wheelspeed;
-            fuelconsumptionrate =  distance / (previousfuel - streams["engineInfo"][11]); // meters / liter
-        } else {
-            fuelconsumptionrate = 0;
-        }
-        previousfuel = streams["engineInfo"][11];
-        timer = 1;
-    }
-    
-    str += "<br> Fuel Consumption:" + (100000 / fuelconsumptionrate).toFixed(5);
     
     this.text.html(str);
 };
