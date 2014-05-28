a4274c062d9a9747d2534badec8c10a5a38c3699
themetalmuncher <themetalmuncher@gmail.com>
2014-05-28 14:10:15 +0100

    [changed] app heights, working nicer now

--- apps/SimpleAGears/app.js
+++ apps/SimpleAGears/app.js
@@ -1,48 +1,46 @@
 function SimpleAGears() {}
 
 SimpleAGears.prototype.initialize = function () {
-	this.canvas = $('<canvas></canvas>').appendTo(this.rootElement);
-		
-	this.canvas.width = 200;
-	this.canvas.height = 30;
+    this.canvas = $('<canvas></canvas>').appendTo(this.rootElement);
 
-	this.loaded = false;
+    this.canvas.width = 200;
+    this.canvas.height = 33;
+
+    this.loaded = false;
 };
 
 SimpleAGears.prototype.update = function (streams) {
-		
-	//Get the values to work with, do rounding and stuff as needed
-	aGear = Math.round(streams["electrics"].gear_A*5);
-	
-	gearNames = ["P","R","N","D","2","1"];
-				
-	//start canvas stuff
-	c = this.canvas[0];
-	ctx = c.getContext('2d');
-	
-	//clear before drawing stuff on canvas
-	ctx.clearRect(0,0,200,30);
-	
-	//Setup Text
-	ctx.font='18px Arial';
-	ctx.textAlign="center";
-	
-	//deselected gears
-	ctx.fillStyle = "RGBA(0,0,0,0.5)";
-	for (i=0; i<6; i++){
-		ctx.fillText(gearNames[i],25+(i*30),20);
-	}
-	
-	//remove stuff over current gear
-	ctx.clearRect((aGear*30)+15,0,20,30);
-	//readd bg
-	ctx.fillStyle = "RGBA(255,255,255,0.75)";
-	ctx.fillRect((aGear*30)+15,4,20,20);
-	
-	//add box around gear
-	ctx.strokeRect((aGear*30)+15,4,20,20);
-	
-	//add current gear back in selected colour
-	ctx.fillStyle = "RGBA(0,0,255,0.5)";
-	ctx.fillText(gearNames[aGear],(aGear*30)+25,20);    
+
+    //Get the values to work with, do rounding and stuff as needed
+    aGear = Math.round(streams["electrics"].gear_A*5);
+
+    gearNames = ["P","R","N","D","2","1"];
+
+    //start canvas stuff
+    c = this.canvas[0];
+    ctx = c.getContext('2d');
+
+    //clear before drawing stuff on canvas
+    ctx.clearRect(0,0,200,33);
+
+    //Setup Text
+    ctx.font='18px Arial';
+    ctx.textAlign="center";
+
+    // //deselected gears
+    ctx.fillStyle = "RGBA(0,0,0,0.5)";
+    for (i=0; i<6; i++){
+        ctx.fillText(gearNames[i],25+(i*30),22);
+    }
+
+    //remove stuff over current gear
+    ctx.fillStyle = "RGBA(255,255,255,0.9)";
+    ctx.fillRect((aGear*30)+15,-2,20,36);
+
+    //add box around gear
+    ctx.strokeRect((aGear*30)+15,-2,20,36);
+
+    //add current gear back in selected colour
+    ctx.fillStyle = "RGBA(0,0,255,0.5)";
+    ctx.fillText(gearNames[aGear],(aGear*30)+25,22);
 };
