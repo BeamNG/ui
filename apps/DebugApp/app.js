22aab069ccc0e79b515c9cc6a9e362be8bf2fde9
theshark <mweigel@beamng.com>
2014-06-01 17:34:15 +0200

    [improved] debugapp

--- apps/DebugApp/app.js
+++ apps/DebugApp/app.js
@@ -1,21 +1,32 @@
 function DebugApp(){}
 
 DebugApp.prototype.initialize = function(){
-	$("<br><br>").appendTo(this.rootElement);
-	this.display1 = $("<span></span>").appendTo(this.rootElement);
-	$("<br><br>").appendTo(this.rootElement);
-	this.display2 = $("<span></span>").appendTo(this.rootElement);
-	$("<br><br>").appendTo(this.rootElement);
-	this.display3 = $("<span></span>").appendTo(this.rootElement);
-
+	this.accordion = $("<div></div>").appendTo(this.rootElement);
+	this.changeVehicle = this.addSection("Change Vehicle");
+	this.levels = this.addSection("Levellist");
+	this.apps = this.addSection("Applist");
+	this.vehicles = this.addSection("Vehiclelist");
+	$("<a>Fullsize</a>").appendTo(this.changeVehicle).button().click(function(event) {
+		beamng.sendGameEngine('chooseVehicle( "fullsize", "vehicles/fullsize/drift.pc", "", 1);');
+	});
+	$("<a>Pickup</a>").appendTo(this.changeVehicle).button().click(function(event) {
+		beamng.sendGameEngine('chooseVehicle( "pickup", "vehicles/pickup/sport.pc", "", 0);');
+	});
 	var self = this;
-	callGameEngineFuncCallback("getLevelList()", function(res){self.display1.html("LevelList: "+JSON.stringify(res));});
-	callGameEngineFuncCallback("getAppList()", function(res){self.display2.html("AppList: "+JSON.stringify(res));});
-	callGameEngineFuncCallback("getVehicleList()", function(res){self.display3.html("VehicleList: "+JSON.stringify(res));});
+	callGameEngineFuncCallback("getLevelList()", function(res){self.levels.html(JSON.stringify(res));});
+	callGameEngineFuncCallback("getAppList()", function(res){self.apps.html(JSON.stringify(res));});
+	callGameEngineFuncCallback("getVehicleList()", function(res){self.vehicles.html(JSON.stringify(res)); self.vehiclelist = res;});
+
+	this.accordion.accordion({heightStyle: "content", collapsible: true});
 };
 
 DebugApp.prototype.update = function(streams){
-	//callLuaFuncCallback("1+1", function(res){self.display2.html("Calculating 1+1 with Lua: "+res);});
+};
+
+DebugApp.prototype.addSection = function(name){
+	$("<h1>"+name+"</h1>").appendTo(this.accordion);
+	var container = $("<div></div>").appendTo(this.accordion);
+	return container;
 };
 
 DebugApp.prototype.resize = function(){
