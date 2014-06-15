02074007f294321d6beec6dfcc87c197f2e0a651
theshark <mweigel@beamng.com>
2014-06-15 13:07:44 +0200

    [added] vehiclechooser (wip)

---
+++ js/dialogs.js
@@ -0,0 +1,93 @@
+var VehicleChooser = (function() {
+	'use strict';
+
+	var mainDiv;
+	
+	var vehicleChoose;
+	var configurationChoose;
+
+	var changeButton;
+
+	var vehicles;
+
+	function init(){
+		callGameEngineFuncCallback("getVehicleList()", function(res){
+			var cChooseBox, vChooseBox;
+			vehicles = res;
+
+			vChooseBox = $('<div><label>Choose Vehicle</label></div>').appendTo(mainDiv);
+			vehicleChoose = $('<select></select>').appendTo(vChooseBox);
+
+			vehicleChoose.change(function(event) {
+				updateConfigurations(vehicleChoose.children('option:selected').attr('value'));
+			});
+
+
+			$.each(vehicles, function(index, val) {
+				$('<option value="'+index+'">'+val[1]+'</option>').appendTo(vehicleChoose);
+			});
+
+			cChooseBox = $('<div><label>Choose Configuration</label></div>').appendTo(mainDiv);
+			configurationChoose = $('<select></select>').appendTo(cChooseBox);
+
+			updateConfigurations(0);
+
+			changeButton = $('<a>Change</a>').appendTo(mainDiv).button().click(function(event) {
+				changeVehicle();
+			});
+		});
+
+
+		mainDiv = $("<div id='vehiclechooser' style='background: #fff'></div>").appendTo("body");
+		mainDiv.dialog({
+			title: "Vehicle Selector",
+			width: 300,
+			height: 200,
+			beforeClose : function(event, ui){
+				close();
+				return false;
+			},
+			resizable: false,
+			closeOnEscape: true
+		});
+		close();
+	}
+
+	function updateConfigurations(vehicle){
+		configurationChoose.empty();
+		$.each(vehicles[vehicle][2], function(index, val) {
+			$('<option value="'+index+'">'+val[1]+'</option>').appendTo(configurationChoose);
+		});
+	}
+
+	function changeVehicle(){
+		var vehicleID = vehicleChoose.children('option:selected').attr('value');
+		var vehicle = vehicles[vehicleID][0];
+		var configurationID = configurationChoose.children('option:selected').attr('value');
+		var configuration = vehicles[vehicleID][2][configurationID][2];
+		console.log(vehicle);
+		console.log(configuration);
+		beamng.sendGameEngine('chooseVehicle( "'+vehicle+'", "'+configuration+'", "", 1);');
+		close();
+	}
+
+	function open(){
+		mainDiv.parent().show();
+		mainDiv.dialog( "moveToTop" );
+	}
+
+	function close(){
+		mainDiv.parent().hide();
+	}
+
+	// run init
+	$(document).ready(function() {
+		init();
+	});
+	// public interface
+	var VehicleChooser = {
+		open: open,
+		close:close
+	};
+	return VehicleChooser;
+}());
