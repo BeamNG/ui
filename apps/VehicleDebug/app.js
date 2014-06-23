5907da3c34a4845acae29836373bc63e60dc4e00
theshark <mweigel@beamng.com>
2014-06-23 15:37:06 +0200

    [improved] Vehicle Debug, each tire can be deflated on its own

--- apps/VehicleDebug/app.js
+++ apps/VehicleDebug/app.js
@@ -12,15 +12,26 @@ VehicleDebug.prototype.initialize = function(){
         beamng.sendActiveObjectLua("beamstate.deflateTires()");
     });
 
-    /*
-    $('<button>Post test message</button>').appendTo(this.rootElement).click(function(){
-        HookManager.trigger('Message', {category: 'bla', msg: 'Hello world', ttl: 10});
-    });
-    */
+    this.dynamicButtonContainer = $('<div></div>').appendTo(this.rootElement);
 
     // TODO: hydros
 };
 
 
 VehicleDebug.prototype.update = function(streams){
+    if(streams.torqueCurve!== undefined){ // <- car/engine changed
+        console.log(this.dynamicButtonContainer);
+        this.dynamicButtonContainer.empty(); // <- delete old Buttons
+
+        var self = this;
+        $.each(streams.wheelInfo, function(index, wheel) { // <- go through the wheels
+             self.addDeflateButton(index, wheel[0]);
+        });
+    }
+};
+
+VehicleDebug.prototype.addDeflateButton = function(number, name){
+    $('<button>Deflate '+name+' Tire</button>').appendTo(this.dynamicButtonContainer).click(function(){
+        beamng.sendActiveObjectLua("beamstate.deflateTire("+number+")");
+    });
 };
