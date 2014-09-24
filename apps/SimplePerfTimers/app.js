7289ff5f121f393e6cc0a918c6646c41e610767e
themetalmuncher <themetalmuncher@gmail.com>
2014-09-24 18:00:51 +0100

    [improved] simpleperftimer app, now has metric/imperial

--- apps/SimplePerfTimers/app.js
+++ apps/SimplePerfTimers/app.js
@@ -1,12 +1,12 @@
 function SimplePerfTimers(){}
 
 SimplePerfTimers.prototype.initialize = function(){
-    this.table = $('<table><thead><tr> <th> <b>Type</b> </th> <th> <b>Start <br>(km/h)</b> </th> <th> <b>End <br>(km/h)</b> </th> <th> <b>Time <br>(s)</b> </th> <th> <b>Distance <br>(m)</b> </th></tr></thead><tbody id="resultstable"><tr></tr></tbody></table>').appendTo(this.rootElement).addClass('table');
+    this.table = $('<table><thead><tr> <th> <b>Type</b> </th> <th> <b>Start</b> </th> <th> <b>End</b> </th> <th> <b>Time</b> </th> <th> <b>Distance</b> </th></tr></thead><tbody id="resultstable"><tr></tr></tbody></table>').appendTo(this.rootElement).addClass('table');
     this.div = $('<div></div>').appendTo(this.rootElement).addClass('div');
 
     this.wheelTimer      = 0;
     this.wheelTimerState = 0;   // 0 - No timer / 1 - Acceleration / 3 - braking
-    this.timerSpeedLimit = 100; // km/h
+    this.timerSpeedLimit = null;
     this.speedMargin     = 0.5;
     this.startPos        = null;
     this.startVelo       = null;
@@ -16,6 +16,21 @@ SimplePerfTimers.prototype.initialize = function(){
 
     this.prevTime        = 0;
     this.curTime         = 0;
+
+
+
+    //If no unit was previously selected, default to km/h
+    if ((this.persistance.Unit != "Imperial") && (this.persistance.Unit != "Metric")) this.persistance.Unit = "Metric";
+
+    var self = this;
+    this.table.click(function(){self.toggleUnit();});
+};
+
+SimplePerfTimers.prototype.toggleUnit = function(){
+    //Toggle between Imperial and Metric, save the option to persistance system
+    this.persistance.Unit = this.persistance.Unit === 'Imperial' ? 'Metric' : 'Imperial';
+	HookManager.trigger('Message',{msg:'Switched SimplePerfTimers to ' + this.persistance.Unit + " speed",ttl: 2});
+    this.save();
 };
 
 function appendToTable (data) {
@@ -24,16 +39,28 @@ function appendToTable (data) {
 
 SimplePerfTimers.prototype.update = function(streams){
 
-    var airspeed = (streams.electrics.airspeed*3.6).toFixed(2);
+
     var throttle = streams.electrics.throttle.toFixed(2);
     var brake    = streams.electrics.brake.toFixed(2);
     var position = streams.sensors.position;
+
+    if (this.persistance.Unit == "Metric"){
+		var airspeed = (streams.electrics.airspeed*3.6).toFixed(2);
+		var speedUnits = "km/h";
+		this.timerSpeedLimit = 100;
+    }
+	else {
+		var airspeed = (streams.electrics.airspeed*2.23693629).toFixed(2);
+		var speedUnits = "MPH";
+		this.timerSpeedLimit = 60;
+	}
+
 
     this.prevTime = this.curTime;
     this.curTime  = performance.now();
     var dt = (this.curTime - this.prevTime)/1000;
 
-    // speed this.testing
+    // speed testing
     if (throttle > 0.5 && airspeed > this.speedMargin && airspeed < this.speedMargin*3 && this.wheelTimerState === 0) {
         this.wheelTimerState = 1;
         this.wheelTimer      = 0;
@@ -51,7 +78,7 @@ SimplePerfTimers.prototype.update = function(streams){
             this.wheelTimerState = 0;
             this.testing         = "";
 
-            appendToTable(["Speed", Math.floor(this.startVelo), Math.floor(this.stopVelo), this.wheelTimer.toFixed(2), distance.toFixed(2)]);
+            appendToTable(["Speed", Math.floor(this.startVelo) + " " + speedUnits, Math.floor(this.stopVelo) + " " + speedUnits, this.wheelTimer.toFixed(2) + " s", distance.toFixed(2) + " m"]);
         }
         if (throttle < 0.5) {
             // console.log("ABORTED");
@@ -60,11 +87,11 @@ SimplePerfTimers.prototype.update = function(streams){
         }
     }
     if (this.wheelTimerState == 1) {
-        this.testing = "Speed Testing... " + airspeed + " km/h, " + this.wheelTimer.toFixed(2) + " s";
+        this.testing = "Speed Testing... " + airspeed + " " + speedUnits + ", " + this.wheelTimer.toFixed(2) + " s";
     }
 
-    //brake this.testing
-    if (brake > 0.5 && airspeed > this.timerSpeedLimit - this.speedMargin && airspeed > this.timerSpeedLimit - this.speedMargin && this.wheelTimerState === 0) {
+    //brake testing
+    if (brake > 0.5 && airspeed > 10 - this.speedMargin && this.wheelTimerState === 0) {
         this.wheelTimerState = 3;
         this.wheelTimer      = 0;
         this.startPos        = position;
@@ -81,7 +108,7 @@ SimplePerfTimers.prototype.update = function(streams){
             this.wheelTimerState = 0;
             this.testing         = "";
 
-            appendToTable(["Brake", Math.floor(this.startVelo), Math.floor(this.stopVelo), this.wheelTimer.toFixed(2), distance]);
+            appendToTable(["Brake", Math.floor(this.startVelo) + " " + speedUnits, Math.floor(this.stopVelo) + " " + speedUnits, this.wheelTimer.toFixed(2) + " s", distance + " m"]);
         }
         if (brake < 0.5) {
             // console.log("ABORTED");
@@ -90,7 +117,7 @@ SimplePerfTimers.prototype.update = function(streams){
         }
     }
     if (this.wheelTimerState == 3) {
-        this.testing = "Brake Testing... " + airspeed + " km/h, " + this.wheelTimer.toFixed(2) + " s";
+        this.testing = "Brake Testing... " + airspeed + " " + speedUnits + ", " + this.wheelTimer.toFixed(2) + " s";
     }
     var str =  this.testing;
     str    += "<br>" + this.aborted;
