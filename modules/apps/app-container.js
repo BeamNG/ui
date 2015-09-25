5f942555c07a7921f6f07191a1c0b494bb0d3b8a
Giorgos Siantikos <siantikosg@gmail.com>
2015-09-25 15:37:03 +0300

    minor apps-related base functions

--- modules/apps/app-container.js
+++ modules/apps/app-container.js
@@ -1,20 +1,62 @@
 angular.module('beamng.apps', [])
 
+.directive('draggable', ['$document', function ($document) {
+  return {
+    link: function (scope, element, attrs) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+
+      element.css({position: 'relative', cursor: 'pointer'});
+
+      element.on('mousedown', function (event) {
+        var active = scope.$eval(attrs.draggable);
+        if (active) {
+          event.preventDefault();
+          startX = event.pageX - x;
+          startY = event.pageY - y;
+          $document.on('mousemove', mousemove);
+          $document.on('mouseup', mouseup);
+        }
+      });
+
+      function mousemove (event) {
+        x = event.pageX - startX;
+        y = event.pageY - startY;
+        element.css({ top: y + 'px', left: x + 'px' });
+      }
+
+      function mouseup () {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      }
+    }
+  };
+}])
+
 .directive('appContainer', ['bngApi', function (bngApi) {
   return {
     restrict: 'E',
     template: '<div ng-transclude style="position:absolute; top: 0; right: 0; left: 0; bottom: 0;"></div>',
     transclude: true,
     replace: true,
-    controller: ['$scope', '$window', function ($scope, $window) {
+    controller: ['$compile', '$element', '$log', '$scope', '$window', function ($compile, $element, $log, $scope, $window) {
 	
       // Just testing ....
       var state = {changes: ['streams'], streams: {sensors: 1}};
       bngApi.activeObjectLua('guiUpdate(' + bngApi.serializeToLua(state) + ')');
+      // ............................
 
       $window.oUpdate = function (streams) {
         $scope.$broadcast('streamsUpdate', streams)
       };
+
+      $scope.$on('spawnApp', function (event, data) {
+        $log.info('[appContainer] receiveed spawnApp w/', data);
+        if (!data.directive) return;
+
+        var el = $compile('<' + data.directive + '></' + data.directive + '>')($scope);
+        $element.append(el);
+      });
+
     }],
 
     link: function (scope, element) {
--- modules/appselect/appselect.js
+++ modules/appselect/appselect.js
@@ -1,7 +1,17 @@
 angular.module('beamng.stuff')
-.controller('AppSelectController', ['$log', 'InstalledMods', function ($log, InstalledMods) {
+.controller('AppSelectController', ['$log', '$rootScope', 'InstalledMods', function ($log, $rootScope, InstalledMods) {
 	var vm = this;
-	vm.list = InstalledMods.apps;
+  vm.list = InstalledMods.apps;
 
-	$log.info('the app list:', vm.list);
+  $log.info('the app list:', vm.list);
+
+  vm.spawn = function (appDirective) {
+    $log.info('[AppSelectController] spawing %s', appDirective);
+    // $rootScope.$broadcast('spawnApp', {directive: appDirective});
+  };
+
+  vm.edit = function () {
+    $log.info('[AppSelectController] going to app-edit mode');
+  };
+
 }]);
